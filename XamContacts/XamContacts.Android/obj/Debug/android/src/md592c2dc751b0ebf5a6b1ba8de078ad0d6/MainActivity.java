4a7df9ec72fe514339320f7d0f7e844fc6d6ec2d
Jaime Hernández <jhernandez.hs@icloud.com>
2018-07-06 00:02:48 -0400

    XamContact curso udemy pcl

---
+++ XamContacts/XamContacts.Android/obj/Debug/android/src/md592c2dc751b0ebf5a6b1ba8de078ad0d6/MainActivity.java
@@ -0,0 +1,47 @@
+package md592c2dc751b0ebf5a6b1ba8de078ad0d6;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XamContacts.Droid.MainActivity, XamContacts.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XamContacts.Droid.MainActivity, XamContacts.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
